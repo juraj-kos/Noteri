18c3811e495866d0abe345cab42130112a59ff05
Juraj Kos <54708032+juraj-kos@users.noreply.github.com>
2020-08-19 14:30:27 +0200

    Made further changes to components, classes with overrides

--- src/components.ts
+++ src/components.ts
@@ -1,34 +1,59 @@
-const Toolbar = (
-	buttonData: string[],
-	containerName: string,
-	prefix: string
-) => {
-	function createElement() {
-		const rootContainer: HTMLElement = document.createElement("div");
-		rootContainer.setAttribute("class", containerName);
-		rootContainer.setAttribute("id", containerName);
-
-		buttonData.forEach((buttonName) => {
-			let buttonDiv: HTMLElement = createButton(
-				`${containerName}-button`,
-				prefix,
-				buttonName
-			);
-			rootContainer.appendChild(buttonDiv);
-		});
+class Toolbar {
+	buildElement(buttonData: string[], containerName: string, prefix: string) {
+		const rootContainer: HTMLElement = this.createRootElement(
+			containerName
+		);
+		this.appendButtons(rootContainer, buttonData, containerName, prefix);
 
 		return rootContainer;
 	}
 
-	function createButton(
-		className: string,
-		prefix: string,
-		buttonName: string
+	createRootElement(containerName: string) {
+		const rootContainer: HTMLElement = document.createElement("div");
+		rootContainer.setAttribute("class", containerName);
+		rootContainer.setAttribute("id", containerName);
+
+		return rootContainer;
+	}
+
+	appendButtons(
+		parentElement: HTMLElement,
+		buttonData: string[],
+		containerName: string,
+		prefix: string
 	) {
+		buttonData.forEach((buttonName) => {
+			let buttonDiv: HTMLElement = this.createButtonElement(
+				`${containerName}-button`,
+				prefix,
+				buttonName
+			);
+			parentElement.appendChild(buttonDiv);
+		});
+	}
+
+	createButtonElement(className: string, prefix: string, buttonName: string) {
 		let buttonDiv: HTMLElement = document.createElement("div");
 		buttonDiv.setAttribute("class", className);
 		buttonDiv.setAttribute("id", `${prefix}-${buttonName}`);
 
+		this.appendSVG(buttonDiv, prefix, buttonName);
+
+		return buttonDiv;
+	}
+
+	appendSVG(parentElement: HTMLElement, prefix: string, buttonName: string) {
+		let { iconString, dimensions } = this.loadSVGString(prefix, buttonName);
+		let updatedIconString = this.updateSVGString(
+			iconString,
+			dimensions,
+			prefix
+		);
+
+		parentElement.innerHTML += updatedIconString;
+	}
+
+	loadSVGString(prefix: string, buttonName: string) {
 		let iconString: string = require(`./Media/Icons/icon-${prefix}-${buttonName}.svg`);
 
 		var viewBoxIndex: number = iconString.indexOf("viewBox");
@@ -38,6 +63,10 @@ const Toolbar = (
 		);
 		var dimensions: string[] = viewBoxString.split(" ");
 
+		return { iconString, dimensions };
+	}
+
+	updateSVGString(iconString: string, dimensions: string[], prefix: string) {
 		iconString =
 			iconString.slice(0, 4) +
 			` width="${dimensions[0]}" height="${dimensions[1]}"` +
@@ -48,21 +77,75 @@ const Toolbar = (
 			` class="${prefix}-icon"` +
 			iconString.slice(4);
 
-		buttonDiv.innerHTML = iconString;
-
-		return buttonDiv;
+		return iconString;
 	}
+}
 
-	return createElement();
-};
-
-const MainToolbar: HTMLElement = Toolbar(
+const MainToolbarInstance: Toolbar = new Toolbar();
+const MainToolbar: HTMLElement = MainToolbarInstance.buildElement(
 	["user", "create", "filter", "sort", "select", "settings"],
 	"main-toolbar",
 	"mtb"
 );
 
-const EditorToolbar: HTMLElement = Toolbar(
+class EditorToolbarClass extends Toolbar {
+	createButtonElement(className: string, prefix: string, buttonName: string) {
+		let buttonDiv: HTMLElement = document.createElement("div");
+		buttonDiv.setAttribute("id", `${prefix}-${buttonName}`);
+
+		if (buttonName === "text-size") {
+			buttonDiv.setAttribute(
+				"class",
+				`${className} button-${buttonName}`
+			);
+			this.appendTextSizeSVG(buttonDiv, prefix, buttonName);
+		} else if (buttonName === "text-align") {
+			buttonDiv.setAttribute(
+				"class",
+				`${className} button-${buttonName}`
+			);
+			let alignNames: string[] = [
+				"align-left",
+				"align-center",
+				"align-right",
+			];
+
+			alignNames.forEach((iconName) => {
+				this.appendSVG(buttonDiv, prefix, iconName);
+			});
+		} else {
+			buttonDiv.setAttribute("class", className);
+			this.appendSVG(buttonDiv, prefix, buttonName);
+		}
+
+		return buttonDiv;
+	}
+
+	appendTextSizeSVG(
+		parentElement: HTMLElement,
+		prefix: string,
+		buttonName: string
+	) {
+		let { iconString, dimensions } = this.loadSVGString(prefix, buttonName);
+
+		for (let index = 0; index < 5; index++) {
+			let [width, height] = dimensions;
+			let newWidth = parseInt(width) * (0.4 + 0.15 * index);
+			let newHeight = parseInt(height) * (0.4 + 0.15 * index);
+			let newDimension = [newWidth.toString(), newHeight.toString()];
+
+			let updatedIconString = this.updateSVGString(
+				iconString,
+				newDimension,
+				prefix
+			);
+
+			parentElement.innerHTML += updatedIconString;
+		}
+	}
+}
+const EditorToolbarInstance: Toolbar = new EditorToolbarClass();
+const EditorToolbar: HTMLElement = EditorToolbarInstance.buildElement(
 	[
 		"text-size",
 		"bold",
@@ -72,7 +155,7 @@ const EditorToolbar: HTMLElement = Toolbar(
 		"number-list",
 		"underline",
 		"strikethrough",
-		"align-left",
+		"text-align",
 		"alert",
 		"checkbox",
 	],
@@ -80,7 +163,8 @@ const EditorToolbar: HTMLElement = Toolbar(
 	"etb"
 );
 
-const NoteToolbar: HTMLElement = Toolbar(
+const NoteToolbarInstance: Toolbar = new Toolbar();
+const NoteToolbar: HTMLElement = NoteToolbarInstance.buildElement(
 	[
 		"tag",
 		"color",
